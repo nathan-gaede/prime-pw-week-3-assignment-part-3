334ec8e98290eb4aca8833a74f8991c1c2e635cf
Nathan Gaede <nathangaede@Nathans-MacBook-Pro.local>
2022-06-19 13:19:00 -0500

    Finished stretch question 7

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -49,6 +49,12 @@ console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop. 
 console.log('7. Showing supplyChanges with "for of" loop');
 
+for (change of supplyChanges) {
+    
+    if(change>0)console.log('Added',change,'parts.');
+    if(change===0)console.log('No Change.');
+    if(change<0)console.log('Removed',change,'parts.');
+}
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
 console.log('8. Total supplies available is:');
