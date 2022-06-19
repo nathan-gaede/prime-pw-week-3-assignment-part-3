d150c5172348fdf539fb3759c43f6bb682f5237b
Nathan Gaede <nathangaede@Nathans-MacBook-Pro.local>
2022-06-19 12:09:45 -0500

    Finished question 6

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -36,6 +36,13 @@ supplyChanges.push(25);
 //    - if the value is negative, format the log as 'Removed x parts.' 
 console.log('6. Showing supplyChanges...');
 
+for (let i=0; i<supplyChanges.length; i += 1) {
+    if (supplyChanges[i]>0)console.log('Added',supplyChanges[i],'parts.')
+    else if (supplyChanges[i]===0)console.log('No Change.')
+    else if (supplyChanges[i]<0)console.log('Removed',supplyChanges[i],'parts.')
+}
+
+
 
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
